4337df3eacc6ac197b693eee12a1219e041f3006
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-08-10 17:38:59 +0900

    middle save

---
+++ Assets/Scripts/Lingua/Lingua.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using NReco.Csv;
+using UnityEditor;
+using UnityEngine;
+
+namespace Lingua
+{
+    public static class Lingua
+    {
+        public enum RegionCode
+        {
+            KR = 0,
+            EN,
+            END
+        }
+
+        private static RegionCode region;
+        public static RegionCode Region
+        {
+            get => region;
+            set
+            {
+                region = value;
+            }
+        }
+
+        static Dictionary<string, string[]> regionTexts;
+
+        static Lingua()
+        {
+            InitRegionTexts();
+        }
+
+        static void InitRegionTexts()
+        {
+            regionTexts = new Dictionary<string, string[]>();
+            var stringTable = Resources.Load<TextAsset>("strings.csv");
+            var inputStream = new MemoryStream(stringTable.bytes);
+            using (var streamRdr = new StreamReader(inputStream))
+            {
+                var csvReader = new CsvReader(streamRdr, ",");
+                string[] strings = new string[(int)RegionCode.END];
+                while (csvReader.Read())
+                {
+                    for (int i = 1; i < csvReader.FieldsCount; i++) 
+                    {
+                        strings[i - 1] = csvReader[i];
+                    }
+                }
+                regionTexts.Add(csvReader[0], strings);
+            }
+        }
+
+        public static string GetString(string key)
+        {
+            return regionTexts[key][(int)Region];
+        }
+    }
+
+}
---
+++ Assets/Scripts/Lingua/LinguaText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Lingua
+{
+    public class LinguaText : MonoBehaviour
+    {
+        private TextMeshPro textObject;
+        string[] strings;
+
+        private void Awake()
+        {
+            textObject = GetComponent<TextMeshPro>();
+        }
+
+        private void OnEnable()
+        {
+
+        }
+
+        void UpdateText()
+        {
+
+        }
+    }
+}
+
