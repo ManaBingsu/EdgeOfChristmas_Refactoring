9df85d10052777200cb554f332eb8b45092e361a
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-08-14 16:24:17 +0900

    Change branch name

---
+++ Assets/Scripts/Achivement/IAchivement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Achivement
+{
+    public interface IAchivement
+    {
+        void Achive()
+        {
+
+        }
+    }
+}
