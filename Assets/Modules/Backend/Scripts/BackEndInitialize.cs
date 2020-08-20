e9403577eccb6109f50504358d8bb676643b0780
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-08-20 12:10:33 +0900

    modify : Block github action

--- Assets/Modules/Backend/Scripts/BackEndInitialize.cs
+++ Assets/Modules/Backend/Scripts/BackEndInitialize.cs
@@ -1,5 +1,6 @@
 ﻿using BackEnd;
 using UnityEngine;
+using Debug = Helper.Debug;
 
 namespace Server.BackEnd
 {
@@ -14,16 +15,20 @@ namespace Server.BackEnd
         {
             if (Backend.IsInitialized)
             {
+                Debug.Log("뒤끝 SDK 초기화 완료");
                 // 구글 해시키 획득
                 if (!Backend.Utils.GetGoogleHash().Equals(""))
                 {
-
+                    Debug.Log($"구글 해시 {Backend.Utils.GetGoogleHash()}");
                 }
+
+                // 서버 시간 획득
+                Debug.Log(Backend.Utils.GetServerTime());
             }
             // 실패 시
             else
             {
-
+                Debug.LogError("뒤끝 SDK 초기화 실패");
             }
         }
     }
