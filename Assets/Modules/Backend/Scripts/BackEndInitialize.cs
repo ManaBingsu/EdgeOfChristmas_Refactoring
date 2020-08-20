4e831380a1372ff03de450cacab5ac8057d197ae
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-08-20 12:02:23 +0900

    feat : Debug helper

---
+++ Assets/Modules/Backend/Scripts/BackEndInitialize.cs
@@ -0,0 +1,31 @@
+﻿using BackEnd;
+using UnityEngine;
+
+namespace Server.BackEnd
+{
+    public class BackEndInitialize : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Backend.Initialize(HandleBackendCallBack);
+        }
+
+        void HandleBackendCallBack()
+        {
+            if (Backend.IsInitialized)
+            {
+                // 구글 해시키 획득
+                if (!Backend.Utils.GetGoogleHash().Equals(""))
+                {
+
+                }
+            }
+            // 실패 시
+            else
+            {
+
+            }
+        }
+    }
+}
+
