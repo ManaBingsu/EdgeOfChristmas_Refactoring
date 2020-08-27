65ebf0d6d2878052be1a3cb6a24d797541529c29
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-08-27 11:18:17 +0900

    style : User login UI scale

---
+++ Assets/Scripts/GameSystem/SceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace GameSystem
+{
+    public class SceneManager : MonoBehaviour
+    {
+        public static SceneManager Instance;
+
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                DestroyImmediate(this.gameObject);
+        }
+    }
+}
+
