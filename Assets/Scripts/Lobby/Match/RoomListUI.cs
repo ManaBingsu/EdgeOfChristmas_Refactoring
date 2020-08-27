73dc097528632cf43cd94d2725e370fdf18bc413
ManaBingsu <SouthPAW Games@DESKTOP-6ISBFO1>
2020-08-27 16:42:23 +0900

    chore : Create sign directory

---
+++ Assets/Scripts/Lobby/Match/RoomListUI.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Lobby
+{
+    public class RoomListUI : MonoBehaviour
+    {
+
+    }
+
+}
